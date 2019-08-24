04cce38e054289b7a8222326546eb79fda99dd9f
Bonnet1 <c.mannion84@gmail.com>
2019-08-24 18:11:06 -0400

    First attempt at creating repeatable app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get  '/new',  to: 'users#new'
+  post '/create',  to: 'users#create'
+  resources :users, only: [:new, :create]
 end
